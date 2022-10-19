8bf2d05035d0b3b0789bf9a631dc10af2869e339
Pavel Fronchik <ridan999@yandex.ru>
2022-10-19 03:59:17 +0300

    22hd

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { ERROR_NOT_FOUND } = require('./utils/utils');
 
 const { PORT = 3000 } = process.env;
 const app = express();
