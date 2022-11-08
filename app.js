c6087c806149a3565f4777be751e00024d065757
Pavel Fronchik <ridan999@yandex.ru>
2022-11-08 23:10:00 +0300

    fixcors

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
+const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const {
   signUp, signIn,
 } = require('./middlewares/validations');
-
-const allowedUrl = 'https://oranlon.nomoredomains.icu';
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -24,6 +23,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(cors);
 app.use(requestLogger);
 app.post('/signin', signIn, login);
 app.post('/signup', signUp, createUser);
@@ -33,14 +33,6 @@ app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
 app.use('*', (req, res, next) => {
-  console.log(req);
-
-  const { origin } = req.headers;
-
-  if (allowedUrl === origin) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
   next(new NotFoundError('Страница не найдена'));
 });
 
