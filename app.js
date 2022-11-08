c1aa51d20f3b0398b1446d42f5b64fb9a5f2430b
Pavel Fronchik <ridan999@yandex.ru>
2022-11-08 22:24:39 +0300

    add Cors

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const {
   signUp, signIn,
 } = require('./middlewares/validations');
+
+const allowedUrl = 'http://oranlon.nomoredomains.icu';
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -21,6 +24,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(requestLogger);
 app.post('/signin', signIn, login);
 app.post('/signup', signUp, createUser);
 
@@ -29,9 +33,16 @@ app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
 app.use('*', (req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedUrl === origin) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   next(new NotFoundError('Страница не найдена'));
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 app.listen(PORT);
